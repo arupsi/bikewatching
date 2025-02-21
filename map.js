cf3fee717c80027b29132814aad1c9512d7917e5
arupsi <arupsi@ucsd.edu>
2025-02-21 14:35:11 +0500

    step 2

--- map.js
+++ map.js
@@ -10,3 +10,40 @@ const map = new mapboxgl.Map({
   minZoom: 5, // Minimum allowed zoom
   maxZoom: 18 // Maximum allowed zoom
 });
+
+map.on('load', () => { 
+    map.addSource('boston_route', {
+        type: 'geojson',
+        data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson?...'
+    });
+
+    map.addLayer({
+        id: 'bike-lanes-b',
+        type: 'line',
+        source: 'boston_route',
+        paint: {
+          'line-color': 'green',
+          'line-width': 2,
+          'line-opacity': 0.5
+        }
+    });
+
+
+    map.addSource('cambridge_route', {
+        type: 'geojson',
+        data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson'
+    });
+
+    map.addLayer({
+        id: 'bike-lanes-c',
+        type: 'line',
+        source: 'cambridge_route',
+        paint: {
+        'line-color': 'green',
+        'line-width': 2,
+        'line-opacity': 0.5
+        }
+    });
+
+});
+
